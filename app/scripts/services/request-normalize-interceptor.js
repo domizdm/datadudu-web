f6265199938804df59199480dd37e0110f422b14
gorebill <gorebill@163.com>
2016-06-07 13:42:06 +0800

    fix bug

--- app/scripts/services/request-normalize-interceptor.js
+++ app/scripts/services/request-normalize-interceptor.js
@@ -8,11 +8,14 @@
  * Factory in the dataduduR3App.
  */
 angular.module('dataduduR3App')
-.factory('RequestNormalizeInterceptor', function ($httpParamSerializerJQLike, $httpParamSerializer) {
+.factory('RequestNormalizeInterceptor', function ($httpParamSerializerJQLike) {
   return {
     request: function(config) {
-      if(/post/i.test(config.method) && config.params['_json'] != true) {
-        config.data = $httpParamSerializerJQLike(config.data);
+      if(/post/i.test(config.method)) {
+        var asTraditionPayload = (!config.params || !config.params._json) ? true : false;
+        if(asTraditionPayload) {
+          config.data = $httpParamSerializerJQLike(config.data);
+        }
       }
       //if(/put/i.test(config.method)){
       //  config.data = $httpParamSerializer(config.data);
