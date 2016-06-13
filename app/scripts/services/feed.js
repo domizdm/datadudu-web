90023efb245c9aca2206168df18e22986b5684f9
gorebill <gorebill@163.com>
2016-06-13 18:04:59 +0800

    fix bug

---
+++ app/scripts/services/feed.js
@@ -0,0 +1,27 @@
+/**
+ * Created by gorebill on 6/13/16.
+ */
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name dataduduR3App.feed
+ * @description
+ * # feed
+ * Service in the dataduduR3App.
+ */
+angular.module('dataduduR3App')
+.service('feed', function ($resource, config) {
+
+  return $resource(config.END_POINT+'/:_action/:id', {
+    id: '@_id'
+  },{
+    addFeed: {
+      method: 'POST',
+      params: {
+        _action: 'update'
+      }
+    }
+  });
+
+});
