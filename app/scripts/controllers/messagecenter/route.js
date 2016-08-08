261aebb428ea38d3194eedd1f5a335d6c0945691
cloudforce3 <peter_zdm@163.com>
2016-08-08 10:36:02 +0800

    #新增在线客服 #新增消息中心页面

---
+++ app/scripts/controllers/messagecenter/route.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name dataduduR3App
+ * @description
+ * # dataduduR3App
+ *
+ * Route module of the application.
+ */
+angular.module('dataduduR3App')
+.config(function ($routeProvider) {
+  $routeProvider
+    .when('/messagecenter',{
+      templateUrl:  'views/messagecenter/message.html',
+      controller:'MessageCtrl'
+    })
+});
+
