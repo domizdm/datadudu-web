5d55fb618d2800192d6f8d3564322417b398496a
gorebill <gorebill@163.com>
2016-08-23 01:28:56 +0800

    added auto-expand first level sidebar related menu when refresh page

--- app/scripts/controllers/sidebar.js
+++ app/scripts/controllers/sidebar.js
@@ -8,6 +8,21 @@
  * Controller of the dataduduR3App
  */
 angular.module('dataduduR3App')
-.controller('SidebarCtrl', function ($scope, config, Auth, $log) {
+.controller('SidebarCtrl', function ($scope, config, Auth, $log, $route, $location) {
   $scope.useDeviceDashboard = config.USE_DEVICE_DASHBOARD;
+
+  $scope.activeNodeWhenIn = function(pathList) {
+    // 加上try避免浏览器对RegExp的不支持,及null list引起的异常
+    try{
+      for(var i=0; i<pathList.length; i++) {
+        var path = pathList[i];
+        var regexp = new RegExp('^/'+path+'(/|\\?|$)', '');
+        if(regexp.test($location.path())) {
+          return true;
+        }
+      }
+    }catch(e){}
+
+    return false;
+  };
 });
