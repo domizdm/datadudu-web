653db4953a430a9def1c89aa72cd0c036e717413
gorebill <gorebill@163.com>
2017-03-01 16:04:30 +0800

    task - review and begin profile module development

---
+++ app/scripts/controllers/groups/group-list.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dataduduR3App.controller:GroupListCtrl
+ * @description
+ * # GroupListCtrl
+ * Controller of the dataduduR3App
+ */
+angular.module('dataduduR3App')
+.controller('GroupListCtrl', function($scope, $q, $filter, $route, $log, $location,
+                                            config, $uibModal, ngNotify, modalConfirm, channel){
+
+
+});
